1bfdb30a066922f14c72fe0046644b323dd0edc7
Ilya_Konon <ilya.konon.95@gmail.com>
2017-02-03 16:17:21 +0300

    passed test for methods #getCurrentPlayerSymbol and #getFieldValue

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,34 +1,52 @@
 class TicTacToe {
     constructor() {
-
+        this.playField = [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null]
+        ];
+        this.currentPlayer = 'x';
     }
 
+    // should return x or o
     getCurrentPlayerSymbol() {
-
+        return this.currentPlayer;
     }
 
+    //should properly update class state (change current player, update marks storage etc.)
     nextTurn(rowIndex, columnIndex) {
-
+        if (this.playField[rowIndex][columnIndex] == null) {
+            this.playField[rowIndex][columnIndex] = this.currentPlayer;
+            if (this.currentPlayer == 'x') {
+                this.currentPlayer = 'o';
+            }
+            else {
+                this.currentPlayer = 'x';
+            }
+        }
     }
 
+    //should return true if game is finished (e.g. there is a winner or it is a draw)
     isFinished() {
-
     }
 
+    //should return winner symbol (x or o) or null if there is no winner yet
     getWinner() {
 
     }
 
+    //should return true if there is no more fields to place a x or o
     noMoreTurns() {
 
     }
 
+    //should return true if there is no more turns and no winner
     isDraw() {
-
     }
 
+    //should return matrix[row][col] value (if any) or null
     getFieldValue(rowIndex, colIndex) {
-
+        return this.playField[rowIndex][colIndex];
     }
 }
 
